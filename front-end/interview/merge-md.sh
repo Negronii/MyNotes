d78ecb67adef346241b832aefe64ba52c4d86e25
Negronii <478258625@qq.com>
2024-03-21 16:46:54 +1100

    prepush and merge-md fix

--- front-end/interview/merge-md.sh
+++ front-end/interview/merge-md.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# set filename for the big md file
+# define the output file
 output="All-In-One.md"
 
-# rm old file
+# delete the output file if it exists
 rm -f $output
 
-# merge all md files
+# merge all markdown files into the output file
 for file in *.md; do
     # skip the output file
     if [[ $file != $output ]]; then
@@ -15,7 +15,11 @@ for file in *.md; do
     fi
 done
 
-# add to git
-git add $output
-git commit -m "update $output"
-git push
+# see if the output file has changed
+if ! git diff --quiet -- $output; then
+    # add the output file to the staging area
+    git add $output
+
+    # commit the output file
+    git commit -m "Automatically merged Markdown files into $output"
+fi
